90d047321505e51749f3d49cb05fe5ee2bb21ad9
Chris Burton <chris@fullstackcaveman.com>
2021-04-16 09:47:40 -0600

    GET all resources endpoint working

--- api/resource/router.js
+++ api/resource/router.js
@@ -3,4 +3,12 @@ const Resource = require('./model');
 
 const router = express.Router();
 
+router.get('/', (req, res, next) => {
+	Resource.getResources()
+		.then((resource) => {
+			res.json(resource);
+		})
+		.catch(next);
+});
+
 module.exports = router;
