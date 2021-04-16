f7ff8bddc79bab90848c4e8ef81b998715d06c7c
Chris Burton <chris@fullstackcaveman.com>
2021-04-16 10:45:25 -0600

    Can get all tasks

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,7 @@
-// build your `Task` model here
+const db = require('../../data/dbConfig');
+
+const getTasks = () => {
+	return db('tasks');
+};
+
+module.exports = { getTasks };
--- api/task/router.js
+++ api/task/router.js
@@ -3,4 +3,12 @@ const Task = require('./model');
 
 const router = express.Router();
 
+router.get('/', (req, res, next) => {
+	Task.getTasks()
+		.then((task) => {
+			res.json(task);
+		})
+		.catch(next);
+});
+
 module.exports = router;
